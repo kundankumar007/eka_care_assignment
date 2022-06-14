584eb8c586094e556893fdb2782281c7b500a6d3
kundan kumar <kundan35708@gmail.com>
2022-06-14 14:02:48 +0530

    assignment modified

--- ekacare/medicalConcepts/models.py
+++ ekacare/medicalConcepts/models.py
@@ -1,21 +1,13 @@
 from random import choices
 from django.db import models
 
-
-class concept(models.Model):
-    contentTypeChoices = [
-        ('diagnosis','Diagnosis'),
-        ('symptom','Symptom'),
-        ('medication','Medication'),
-        ('lab test', 'Lab test'),
-        ]
-    name = models.CharField(max_length = 30 ,blank = False)
-    concept_type = models.CharField(max_length = 50,blank = False,choices = contentTypeChoices,default = 'diagnosis')
-    doctors_name = models.CharField(max_length = 30,blank = False)
-    created_at = models.DateTimeField(auto_now_add = True)
-    modified_at = models.DateField (auto_now = True)
+class concept_type(models.Model):
+    
+   
+    id = models.BigAutoField(primary_key = True)
+    concept_type_name = models.CharField(max_length = 50,blank = False)
     def __str__(self):
-        return self.name
+        return self.concept_type_name
 
 class doctor(models.Model):
      id = models.IntegerField(primary_key = True)
@@ -23,18 +15,20 @@ class doctor(models.Model):
      def __str__(self):
         return self.name
 
-class concept_type(models.Model):
-    contentTypeChoices = [
-        ('diagnosis','Diagnosis'),
-        ('symptom','Symptom'),
-        ('medication','Medication'),
-        ('lab test', 'Lab test'),
-    ]
-   
-    id = models.BigAutoField(primary_key = True)
-    concept_type_name = models.CharField(max_length = 50,blank = False,choices = contentTypeChoices,default = 'diagnosis')
+
+class concept(models.Model):
+
+    name = models.CharField(max_length = 30 ,blank = False)
+    concept_type = models.ForeignKey(concept_type, on_delete=models.CASCADE)
+    doctors_name = models.ForeignKey(doctor, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateField (auto_now = True)
     def __str__(self):
-        return self.concept_type_name
+        return self.name
+    
+
+
+
 
 
 
